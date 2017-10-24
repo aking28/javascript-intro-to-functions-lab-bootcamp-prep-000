ae91b662444002c4169f87f541579a880c7945bb
Alexandra King <aking28@me.com>
2017-10-24 15:30:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-describe('shout(string)', () => { ... })
 function shout(string) {
 }
-function shout(string) {
-  return string
-}
-'Hello!'.toUpperCase()
-function shout(string) {
-  return string.toUpperCase()
-}
+
+
